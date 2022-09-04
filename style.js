d712e8bbb777da72ce0d445537650a50149a5a54
Valentino <91152648+00tino@users.noreply.github.com>
2022-09-04 21:16:27 +0200

    Update style.js

--- style.js
+++ style.js
@@ -1,22 +1,22 @@
-const display1El = document.querySelector('.display-1')
-const display2El = document.querySelector('.display-2')
-const tempResultEl = document.querySelector('.temp-result')
-const numbersEl = document.querySelectorAll('.number')
-const operationEl = document.querySelectorAll('.operation')
-const equalEl = document.querySelector('.equal')
-const clearAllEl = document.querySelector('.all-clear')
-const clearLastEl = document.querySelector('.last-entity-clear')
-let dis1Num = ''
-let dis2Num = ''
+const display1El = document.querySelector(".display-1")
+const display2El = document.querySelector(".display-2")
+const tempResultEl = document.querySelector(".temp-result")
+const numbersEl = document.querySelectorAll(".number")
+const operationEl = document.querySelectorAll(".operation")
+const equalEl = document.querySelector(".equal")
+const clearAllEl = document.querySelector(".all-clear")
+const clearLastEl = document.querySelector(".last-entity-clear")
+let dis1Num = ""
+let dis2Num = ""
 let result = null
-let lastOperation = ''
+let lastOperation = ""
 let haveDot = false
 
 numbersEl.forEach((number) => {
-  number.addEventListener('click', (e) => {
+  number.addEventListener("click", (e) => {
     if (e.target.innerText === '.' && !haveDot) {
       haveDot = true
-    } else if (e.target.innerText === '.' && haveDot) {
+    } else if (e.target.innerText === "." && haveDot) {
       return
     }
     dis2Num += e.target.innerText
@@ -26,7 +26,7 @@ numbersEl.forEach((number) => {
 })
 
 operationEl.forEach((operation) => {
-  operation.addEventListener('click', (e) => {
+  operation.addEventListener("click", (e) => {
     if (!dis2Num) return
     haveDot = false
     const operationName = e.target.innerText
@@ -40,76 +40,76 @@ operationEl.forEach((operation) => {
     console.log(result)
   })
 })
-function clearVar(name = '') {
-  dis1Num += dis2Num + ' ' + name + ' '
+function clearVar(name = "") {
+  dis1Num += dis2Num + " " + name + " "
   display1El.innerText = dis1Num
-  display2El.innerText = ''
-  dis2Num = ''
+  display2El.innerText = ""
+  dis2Num = ""
   tempResultEl.innerText = result
 }
 
 function mathOperation() {
-  if (lastOperation === 'x') {
+  if (lastOperation === "x") {
     result = parseFloat(result) * parseFloat(dis2Num)
-  } else if (lastOperation === '+') {
+  } else if (lastOperation === "+") {
     result = parseFloat(result) + parseFloat(dis2Num)
-  } else if (lastOperation === '-') {
+  } else if (lastOperation === "-") {
     result = parseFloat(result) - parseFloat(dis2Num)
-  } else if (lastOperation === '/') {
+  } else if (lastOperation === "/") {
     result = parseFloat(result) / parseFloat(dis2Num)
-  } else if (lastOperation === '%') {
+  } else if (lastOperation === "%") {
     result = parseFloat(result) % parseFloat(dis2Num)
   }
 }
 // operation();
 
-equalEl.addEventListener('click', () => {
+equalEl.addEventListener("click", () => {
   if (!dis2Num || !dis1Num) return
   haveDot = false
   mathOperation()
   clearVar()
   display2El.innerText = result
-  tempResultEl.innerText = ''
+  tempResultEl.innerText = ""
   dis2Num = result
-  dis1Num = ''
+  dis1Num = ""
 })
 
-clearAllEl.addEventListener('click', () => {
-  dis1Num = ''
-  dis2Num = ''
-  display1El.innerText = ''
-  display2El.innerText = ''
-  result = ''
-  tempResultEl.innerText = ''
+clearAllEl.addEventListener("click", () => {
+  dis1Num = ""
+  dis2Num = ""
+  display1El.innerText = ""
+  display2El.innerText = ""
+  result = ""
+  tempResultEl.innerText = ""
 })
 
-clearLastEl.addEventListener('click', () => {
-  display2El.innerText = ''
-  dis2Num = ''
+clearLastEl.addEventListener("click", () => {
+  display2El.innerText = ""
+  dis2Num = ""
 })
 
-window.addEventListener('keydown', (e) => {
+window.addEventListener("keydown", (e) => {
   if (
-    e.key === '0' ||
-    e.key === '1' ||
-    e.key === '2' ||
-    e.key === '3' ||
-    e.key === '4' ||
-    e.key === '5' ||
-    e.key === '6' ||
-    e.key === '7' ||
-    e.key === '8' ||
-    e.key === '9' ||
-    e.key === '.'
+    e.key === "0" ||
+    e.key === "1" ||
+    e.key === "2" ||
+    e.key === "3" ||
+    e.key === "4" ||
+    e.key === "5" ||
+    e.key === "6" ||
+    e.key === "7" ||
+    e.key === "8" ||
+    e.key === "9" ||
+    e.key === "."
   ) {
     clickButtonEl(e.key)
     // console.log(e.key)
-  } else if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === '%') {
+  } else if (e.key === "+" || e.key === "-" || e.key === "/" || e.key === "%") {
     clickOperation(e.key)
-  } else if (e.key === '*') {
-    clickOperation('x')
+  } else if (e.key === "*") {
+    clickOperation("x")
     // console.log(e.key)
-  } else if (e.key == 'Enter' || e.key === '=') {
+  } else if (e.key == "Enter" || e.key === "=") {
     clickEqual()
   }
   // console.log(e.key)
